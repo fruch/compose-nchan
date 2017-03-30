fa4d8f05fb98a63e81f4275c752d3c6aec2e851b
Israel Fruchter <ifruchte@cisco.com>
2017-03-30 23:20:22 +0300

    first commit, not yet working

---
+++ locustfile.py
@@ -0,0 +1,25 @@
+import uuid
+
+from locust import HttpLocust, TaskSet
+
+def sub(l):
+    with l.client.get("/sub/" + l.user_id, catch_response=True) as response:
+        if response.status_code == 200:
+            response.success()
+        else:
+            response.failure("%d, %s" % (response.status_code, response.content))
+
+def pub(l):
+    pass
+
+class UserBehavior(TaskSet):
+    tasks = {sub: 3, pub: 1}
+
+
+    def on_start(self):
+        self.user_id = str(uuid.uuid4())
+
+class LongPollUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 100
+    max_wait = 300
---
+++ run.sh
@@ -0,0 +1,1 @@
+locust  --host=http://127.0.0.1:8087  --print-stats -c 10000 -r 100 --no-web
